66efcd8a88914b4e98eaa10772d2a54c32ba95a7
Mohammed Abdul Jawad Vajeeh <51875089+jawadsab@users.noreply.github.com>
2021-01-25 21:58:40 +0530

    Added MenuItems

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { MenuItems } from "./MenuItems";
 
 const Navbar = () => {
-  return <nav className="Navbar"></nav>;
+  return (
+    <nav className="Navbar">
+      <h1 className="navbar-logo">React</h1>
+      <div className="menu-icon"></div>
+      <ul>
+        <li>
+          <a href="#"></a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
