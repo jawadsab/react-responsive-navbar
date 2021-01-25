ff6141b5611bd04478b5acebb9e24ef48c58a7c3
Mohammed Abdul Jawad Vajeeh <51875089+jawadsab@users.noreply.github.com>
2021-01-25 22:38:05 +0530

    Styles navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./styles.css";
 import Navbar from "./components/Navbar/Navbar";
 
 export default function App() {
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { MenuItems } from "./MenuItems";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [mobileView, setMobileView] = useState(false);
+  const handleClick = () => {
+    setMobileView(!mobileView);
+  };
   return (
     <nav className="Navbar">
       <h1 className="navbar-logo">
         React <i className="fab fa-react"></i>
       </h1>
-      <div className="menu-icon"></div>
-      <ul>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={mobileView ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className={mobileView ? "nav-menu active" : "nav-menu"}>
         {MenuItems.map((item, index) => {
           return (
             <li key={index}>
